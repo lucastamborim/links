30a0c3b9f31ad5472efe0b365c6295051c0a4bb9
Lucas Tamborim <lucas.silva@datacake.com.br>
2026-01-23 14:48:51 -0300

    feat: Cria componente Category

--- src/app/index/index.tsx
+++ src/app/index/index.tsx
@@ -1,3 +1,4 @@
+import { Category } from "@/components/category";
 import { colors } from "@/styles/colors";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Image, TouchableOpacity, View } from "react-native";
@@ -12,6 +13,7 @@ export default function Index() {
           <MaterialIcons name="add" size={32} color={colors.green[300]} />
         </TouchableOpacity>
       </View>
+      <Category />
     </View>
   );
 }
